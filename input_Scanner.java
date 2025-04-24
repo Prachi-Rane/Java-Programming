1b2b0cc66966eb7df42a5c9b00def7ae5541fa6c
Prachi Deepak Rane <181490459+Prachi-Rane@users.noreply.github.com>
2025-04-24 23:05:57 +0530

    Add files via upload

---
+++ input_Scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class input_add{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number: ");
+a=sc.nextInt();
+System.out.println("Enter a NUmber: ");
+b=sc.nextInt();
+c=a+b;
+System.out.println("Addition is"+c);
+}
+}
