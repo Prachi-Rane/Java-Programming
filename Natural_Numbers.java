c64f07d7f2e98e23f852e3e9a30a6534a55e6d61
Prachi Deepak Rane <181490459+Prachi-Rane@users.noreply.github.com>
2025-04-28 19:53:35 +0530

    Add files via upload

---
+++ Natural_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class natural
+{
+public static void main(String args[])
+{
+int i,n;
+Scanner sc =new Scanner(System.in);
+i=1;
+System.out.println("Enter a Number:");
+n=sc.nextInt();
+while(i<=n)
+{
+System.out.println(i);
+i=i+1;
+}
+}
+}
